e0267e8ce72489c237e439d53381054c07c59410
Hagen-Kwik <hagen.kwik@gmail.com>
2022-10-27 00:21:54 +0700

    so far

--- AFL BumiComp/BumiComp/resources/views/layout/homelayout.blade.php
+++ AFL BumiComp/BumiComp/resources/views/layout/homelayout.blade.php
@@ -7,22 +7,41 @@
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <link rel="stylesheet" href="css/index.css" /> 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+
 </head>
   <body>
     
-      <header class="masthead">
-        <div class="container h-100">
-          <div class="row h-100 align-items-center">
-            <div class="col-12 text-center">
-              @yield('header')
-            </div>
-          </div>
-        </div>
-      </header>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
-<h1><?=$title;?></h1>
     <div>
-        @yield('container')
+      @include('components.header')
+
+    {{-- for text to be int the middle of the navigation --}}
+
+        <div class="masthead">
+          <div class="container h-100">
+            <div class="row h-100 align-items-center">
+              <div class="col-12 text-center">
+                @yield('navbar')
+             </div>
+           </div>
+         </div>
+      </div>
+
     </div>
+
+
+
+
+
+    <div>
+        @yield('body')
+    </div>
+
+
+
+
+    @include('components.footer')
+
+
 </body>
 </html>
