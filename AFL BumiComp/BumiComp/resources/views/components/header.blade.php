ba0181542c690c662f49bdc164d6aca8f64acce8
Hagen-Kwik <hagen.kwik@gmail.com>
2022-10-26 20:08:48 +0700

    NEW

---
+++ AFL BumiComp/BumiComp/resources/views/components/header.blade.php
@@ -0,0 +1,32 @@
+<nav class="navbar bg-dark navbar-expand-lg">
+    <div class="container-fluid ">
+      <a class="navbar-brand text-white" href="#">BMC</a>
+      <div class="w-75 d-flex justify-content-end">
+        <button class="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+      </div>
+      <div class="d-flex flex-right">
+        <div class="collapse navbar-collapse w-100" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active text-white" aria-current="page" href="/">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-white" href="/about">About</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Gadgets
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Hardisk</a></li>
+                  <li><a class="dropdown-item" href="#">PC & Laptop</a></li>
+                  <li><a class="dropdown-item" href="#">Printer</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+      </div>     
+    </div>
+  </nav>
