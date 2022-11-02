ca145a71983defb41d50c3c61b6217cb9615f5ae
Hagen-Kwik <hagen.kwik@gmail.com>
2022-11-02 12:35:59 +0700

    abnout us selesai

---
+++ AFL BumiComp/BumiComp/resources/views/about.blade.php
@@ -0,0 +1,91 @@
+@extends('layout.homelayout')
+
+@section('body')
+    <!-- ======= Contact Section ======= -->
+
+
+    <section id="contact">
+        <div class="container">
+            <div class="section-header">
+                <h1 class="section-title m-5">Our History</h3>
+                    <div class="row">
+                        <div class="col-4">
+                            <img src="image/about us/toko kami.png" alt="" class="img-fluid">
+                        </div>
+                        <div class="col-8">
+                            <p class="section-description">Toko Bumi Makmur Computer berdiri dari tahun 1997. Toko kami sudah
+                                menyediakan jasa Service dan juga menjual aksesoris komputer dan juga sparepart komputer dan
+                                printer. Jasa service yang kami sediakan adalah service cpu, printer, dan berfokuskan kepada
+                                service
+                                HDD (Penarikan data) </p>
+                            <p class="section-description">Saat ini kami sudah bekerja sama dengan Indodata Recovery untuk
+                                menjadi
+                                cabang repair center mereka
+                                di Makassar (Indonesia Timur)</p>
+                        </div>
+                    </div>
+            </div>
+        </div>
+
+        <div class="mt-5" data-aos="fade-up">
+            <div class="row text-center">
+                <div class="col-6">
+                    <div class="row mb-5">
+                        <div class="col-lg-6 col-md-6" data-aos="zoom-in">
+                            <div>
+                                <div class="d-flex justify-content-center mb-3">
+                                    <img src="image/about us/pinpoint.png" alt="" class="pic">
+                                </div>
+                                <h4 class="title">Lokasi: </h4>
+                                <p class="description">Jalan Tentara Pelajar No.124, Melayu, Makassar City, Sulawesi Selatan
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-md-6" data-aos="zoom-in">
+                            <div>
+                                <div class="d-flex justify-content-center mb-3">
+                                    <img src="image/about us/time.png" alt="" class="pic">
+                                </div>
+                                <h4 class="title"> Buka & Tutup: </h4>
+                                <p class="description">Monday - Saturday:
+                                    10.50 - 5.30
+                                    Sunday & Public Holiday: Closed</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mt-5">
+                        <div class="col-lg-6 col-md-6" data-aos="zoom-in">
+                            <div>
+                                <div class="d-flex justify-content-center mb-3">
+                                    <img src="image/about us/phone.png" alt="" class="pic">
+                                </div>
+                                <h4 class="title">Kontak: </h4>
+                                <p class="description">081 - 24217937</p>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-6 col-md-6" data-aos="zoom-in">
+                            <div>
+                                <div class="d-flex justify-content-center mb-3">
+                                    <img src="image/about us/mail.png" alt="" class="pic">
+                                </div>
+                                <h4 class="title">Email:</h4>
+                                <p class="description">bumihoras@gmail.com</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-6">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d22864.11283411948!2d-73.96468908098944!3d40.630720240038435!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew+York%2C+NY%2C+USA!5e0!3m2!1sen!2sbg!4v1540447494452"
+                        width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+
+
+    </section>
+    <!-- End Contact Section -->
+@endsection
